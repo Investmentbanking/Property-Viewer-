3a1296e95ab8b57322b821de5b8a1a23941ec623
fox <unconfigured@null.spigotmc.org>
2022-03-23 03:56:33 +0000

    Added Comments and cleaned code

--- src/main/java/InspectListingMenu.java
+++ src/main/java/InspectListingMenu.java
@@ -2,12 +2,8 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -18,7 +14,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 /**
- * A pane class which displays all the listings in the form of ListingBox's.
+ * A ListView which displays all the listings in the form of ListingBox's.
  * This is a list which the user can scroll to view all af the Boxes.
  *
  * @author Cosmo Colman (K21090628)
@@ -28,24 +24,20 @@ public class InspectListingMenu extends ListView<HBox> {
 
     private final int SPACING = 10;         // Spacing between boxes.
     private final int ROW_MAX = 6;          // How many boxes per row.
-
-    private final int STARTING_BOUND = 10;  // The amount of rows which are loaded on start.
     private final int SELECT_BOUND = 4;     // When mouse hover on row, this is how many rows above and below are also checked.
+    private final DoubleProperty SIZE_PROPERTY = new SimpleDoubleProperty();    // Property for resize of boxes.
 
-    private ArrayList<NewAirbnbListing> currentListings;
-    private ArrayList<HBox> rows;
-    private HashMap<HBox, Boolean> imageShown;
-    private static HashMap<NewAirbnbListing, ListingBox> boxLookup;
-
+    // List sort options
     public static final ObservableList<String> SORT_OPTIONS = FXCollections.observableArrayList( "Listing Name", "Host Name", "Review Score", "Price");
     public static final ObservableList<String> ORDER_OPTIONS = FXCollections.observableArrayList("Ascending", "Descending");
-
     private static String sortSelected = SORT_OPTIONS.get(0);
     private static String orderSelected = ORDER_OPTIONS.get(0);
-
     private static boolean showOutOfRange = true;
 
-    private final DoubleProperty size = new SimpleDoubleProperty();
+    private final ArrayList<NewAirbnbListing> currentListings;
+    private ArrayList<HBox> rows;
+    //private HashMap<HBox, Boolean> imageShown;
+    private static HashMap<NewAirbnbListing, ListingBox> boxLookup;
 
     /**
      * Constructor for a listing menu of boxes displaying properties.
@@ -54,12 +46,12 @@ public class InspectListingMenu extends ListView<HBox> {
     public InspectListingMenu(ArrayList<NewAirbnbListing> listings) {
         currentListings = listings;
 
+        // De-loads off-screen images so the new images can load.
         if(boxLookup != null) {
-            boxLookup.forEach((key, value) -> {
-                value.cancelLoad();                     // De-loads off-screen images so the new images can load.
-            });
+            boxLookup.forEach((key, value) -> value.cancelLoad());
         }
 
+        // Creates all the ListingBoxes for each listing.
         boxLookup = new HashMap<>();
         for (NewAirbnbListing listing : listings){
             ListingBox box = new ListingBox(listing);
@@ -106,10 +98,10 @@ public class InspectListingMenu extends ListView<HBox> {
 
     /**
      * Generates all the components and properties of the list.
-     * @param listings The list of properties
+     * @param listings The listing in the order to be generated.
      */
     private void generate(ArrayList<NewAirbnbListing> listings){
-        sortList(listings);                                                         // Order the list before splitting out of range values.
+        sortList(listings);                                                          // Order the list before splitting out of range values.
 
         // Split listing with in-range and out-of-range
         ArrayList<NewAirbnbListing> inRangeListings = new ArrayList<>();
@@ -122,9 +114,7 @@ public class InspectListingMenu extends ListView<HBox> {
         getItems().addAll(rows);
 
         double padding = 40;
-        boundsInLocalProperty().addListener((obs, old, bounds) -> {
-            size.setValue((bounds.getWidth() - padding - ((ROW_MAX - 1) * SPACING)) / ROW_MAX);
-        });
+        boundsInLocalProperty().addListener((obs, old, bounds) -> SIZE_PROPERTY.setValue((bounds.getWidth() - padding - ((ROW_MAX - 1) * SPACING)) / ROW_MAX));
 
 //        for (int i = 0; i <= STARTING_BOUND; i++){
 //            showImageInRow(rows.get(i));                // Loads image of first SELECT_BOUND mount of rows.
@@ -138,16 +128,16 @@ public class InspectListingMenu extends ListView<HBox> {
     private void sortList(ArrayList<NewAirbnbListing> listings){
         switch(SORT_OPTIONS.indexOf(sortSelected)){
             case 0: // Listing Name
-                listings.sort(Comparator.comparing(NewAirbnbListing::getName));                 // Ascending
+                listings.sort(Comparator.comparing(NewAirbnbListing::getName));                 // Ascending Order
                 break;
             case 1: // Host Name
-                listings.sort(Comparator.comparing(NewAirbnbListing::getHostName));             // Ascending
+                listings.sort(Comparator.comparing(NewAirbnbListing::getHostName));             // Ascending Order
                 break;
             case 2: // Review Score
-                listings.sort(Comparator.comparing(NewAirbnbListing::getReviewScoresRating));   // Ascending
+                listings.sort(Comparator.comparing(NewAirbnbListing::getReviewScoresRating));   // Ascending Order
                 break;
             case 3: // Price
-                listings.sort(Comparator.comparing(NewAirbnbListing::getPrice));                // Ascending
+                listings.sort(Comparator.comparing(NewAirbnbListing::getPrice));                // Ascending Order
                 break;
         }
 
@@ -180,16 +170,14 @@ public class InspectListingMenu extends ListView<HBox> {
      * @param outOfRange The listings out of the selected range.
      */
     private void initialiseBoxes(ArrayList<NewAirbnbListing> inRange, ArrayList<NewAirbnbListing> outOfRange){
-        // Prepare Boxes and Rows
-        rows = new ArrayList<>();
-        //imageShown = new HashMap<>();
-
-        // Create List
+        // Combine lists to make new list that will be displayed.
         int inRangeCount = inRange.size();
         ArrayList<NewAirbnbListing> newListing = new ArrayList<>();
         newListing.addAll(inRange);
         newListing.addAll(outOfRange);
 
+        //imageShown = new HashMap<>();
+        rows = new ArrayList<>();
         HBox row = null;
         int rowCount = 0;
         int listingCount = 0;
@@ -201,23 +189,19 @@ public class InspectListingMenu extends ListView<HBox> {
                 row.setPadding(new Insets((((double)SPACING)/4), 0, (((double)SPACING)/4), 0));
                 //imageShown.put(row, false);
                 rows.add(row);
-                //root.getChildren().add(row);
                 rowCount = 0;
             }
 
             ListingBox box = boxLookup.get(listing);
-            if (listingCount < inRangeCount) {
-                box.setInvalidColour(false);
-            }
-            else {
-                box.setInvalidColour(true);
-            }
+            box.setInvalidColour(listingCount >= inRangeCount);
             box.showImage();
 
-            box.minWidthProperty().bind(size);
+            box.minWidthProperty().bind(SIZE_PROPERTY);
             box.setOnMouseClicked(this::openInspectMenu);
+
             row.getChildren().add(box);
             HBox.setHgrow(box, Priority.ALWAYS);
+
             rowCount++;
             listingCount++;
         }
@@ -236,42 +220,42 @@ public class InspectListingMenu extends ListView<HBox> {
 
     // ALL BELOW MAY BE DELETE OR REPURPOSED
 
-    public void showRowImages(MouseEvent event){
-        HBox hBox = ((HBox)(event.getSource()));
-        showImageInRow(hBox);
-
-        int index = rows.indexOf(hBox);
-        int ok = -1;
-        for (int i = 1; i < SELECT_BOUND; i++){
-            if ((index - i) >= 0){
-                showImageInRow(rows.get(index - i));
-            }
-            if ((index + i) < rows.size()){
-                showImageInRow(rows.get(index + i));
-            }
-        }
-    }
-
-    private void showImageInRow(HBox row){
-        if (!imageShown.get(row)) {
-            imageShown.replace(row, true);
-            for (Node node : row.getChildrenUnmodifiable()) {
-                ((ListingBox) node).showImage();
-            }
-        }
-    }
-
-    public void showOnScreenImages(){
-        Bounds paneBounds = localToScene(getBoundsInParent());
-        for (HBox hBox : rows){
-            Bounds nodeBounds = hBox.localToScene(hBox.getBoundsInLocal());
-            if(paneBounds.intersects(nodeBounds)){
-                imageShown.replace(hBox, true);
-                for (Node node : hBox.getChildrenUnmodifiable()){
-                    //System.out.println("yes");
-                    ((ListingBox)node).showImage();
-                }
-            }
+//    public void showRowImages(MouseEvent event){
+//        HBox hBox = ((HBox)(event.getSource()));
+//        showImageInRow(hBox);
+//
+//        int index = rows.indexOf(hBox);
+//        int ok = -1;
+//        for (int i = 1; i < SELECT_BOUND; i++){
+//            if ((index - i) >= 0){
+//                showImageInRow(rows.get(index - i));
+//            }
+//            if ((index + i) < rows.size()){
+//                showImageInRow(rows.get(index + i));
+//            }
+//        }
+//    }
+//
+//    private void showImageInRow(HBox row){
+//        if (!imageShown.get(row)) {
+//            imageShown.replace(row, true);
+//            for (Node node : row.getChildrenUnmodifiable()) {
+//                ((ListingBox) node).showImage();
+//            }
+//        }
+//    }
+//
+//    public void showOnScreenImages(){
+//        Bounds paneBounds = localToScene(getBoundsInParent());
+//        for (HBox hBox : rows){
+//            Bounds nodeBounds = hBox.localToScene(hBox.getBoundsInLocal());
+//            if(paneBounds.intersects(nodeBounds)){
+//                imageShown.replace(hBox, true);
+//                for (Node node : hBox.getChildrenUnmodifiable()){
+//                    //System.out.println("yes");
+//                    ((ListingBox)node).showImage();
+//                }
+//            }
 //            else {
 //                for (Node node : hBox.getChildrenUnmodifiable()){
 //                    //System.out.println("no");
@@ -292,21 +276,21 @@ public class InspectListingMenu extends ListView<HBox> {
 //            else{
 //                break;
 //            }
-        }
-    }
-
-    // Credit to https://stackoverflow.com/a/30780960/11245518
-    private ArrayList<Node> getVisibleNodes(ScrollPane pane) {
-        ArrayList<Node> visibleNodes = new ArrayList<>();
-        Bounds paneBounds = pane.localToScene(pane.getBoundsInParent());
-        if (pane.getContent() instanceof Parent) {
-            for (Node n : ((Parent) pane.getContent()).getChildrenUnmodifiable()) {
-                Bounds nodeBounds = n.localToScene(n.getBoundsInLocal());
-                if (paneBounds.intersects(nodeBounds)) {
-                    visibleNodes.add(n);
-                }
-            }
-        }
-        return visibleNodes;
-    }
+//        }
+//    }
+//
+//    // Credit to https://stackoverflow.com/a/30780960/11245518
+//    private ArrayList<Node> getVisibleNodes(ScrollPane pane) {
+//        ArrayList<Node> visibleNodes = new ArrayList<>();
+//        Bounds paneBounds = pane.localToScene(pane.getBoundsInParent());
+//        if (pane.getContent() instanceof Parent) {
+//            for (Node n : ((Parent) pane.getContent()).getChildrenUnmodifiable()) {
+//                Bounds nodeBounds = n.localToScene(n.getBoundsInLocal());
+//                if (paneBounds.intersects(nodeBounds)) {
+//                    visibleNodes.add(n);
+//                }
+//            }
+//        }
+//        return visibleNodes;
+//    }
 }
