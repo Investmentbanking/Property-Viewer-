b078a2c961dc55226d18a54997bd6af6391011d8
fox <unconfigured@null.spigotmc.org>
2022-03-23 05:01:16 +0000

    Added Comments

--- src/main/java/MenuCircle.java
+++ src/main/java/MenuCircle.java
@@ -17,21 +17,19 @@ import java.util.ArrayList;
  * A pane of a circle that represents a borough. The size represents the amount of available listings and the colour represents the average price.
  *
  * @author Cosmo Colman (K21090628)
- * @version 22.03.2022
+ * @version 23.03.2022
  */
 public class MenuCircle extends StackPane {
 
     private Borough borough;
     private ArrayList<NewAirbnbListing> listings;
 
-    //private Pane shapePane;
     private Circle circle;
     private Text text;
 
     private double circleSize;
     private Color circleColor;
 
-    //private static final Color CHEAP_COLOR = Color.rgb(74, 222, 16);
     public static final Color CHEAP_COLOR = Color.rgb(46, 50, 177);
     public static final Color EXPENSIVE_COLOR = Color.rgb(242, 63, 63);
 
@@ -46,8 +44,11 @@ public class MenuCircle extends StackPane {
     private FillTransition filltCircleIn, filltCircleOut;
     private ScaleTransition stTextIn, stTextOut;
 
-    private static ArrayList<MenuCircle> allCircles;
-
+    /**
+     * Constructor for a Menu-Circle which represents a specific borough and contains the listing of all properties in that borough. Clicking this object will open the pane specific to that borough.
+     * @param borough The borough the object represents.
+     * @param listings The listings of that borough.
+     */
     public MenuCircle(Borough borough, ArrayList<NewAirbnbListing> listings) {
         this.borough = borough;
         this.listings = listings;
@@ -74,44 +75,11 @@ public class MenuCircle extends StackPane {
         stTextOut.setToX(1);
         stTextOut.setToY(1);
 
-
         circle.setOnMouseEntered(this::mouseEnter);
         circle.setOnMouseExited(this::mouseLeave);
 
-        //setOnMouseDragged(this::moveToMouse);       // FOR TESTING
+        getChildren().addAll(circle);
 
-        getChildren().addAll(circle);//,text);
-
-
-
-
-//        circle.radiusProperty().bind(widthProperty());
-
-        putTextInCircleNEW();
-
-        // Visualise pane test
-        //setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-    }
-
-
-
-    private void mouseEnter(MouseEvent event){
-        setCursor(Cursor.HAND);
-        filltCircleIn.play();
-        stTextIn.play();
-    }
-
-    private void mouseLeave(MouseEvent event){
-        setCursor(Cursor.DEFAULT);
-        filltCircleOut.play();
-        stTextOut.play();
-    }
-
-    private void openInspectionWindow(MouseEvent event){
-       InspectMenuController.create(listings, borough.getName());
-    }
-
-    private void putTextInCircleNEW(){
         do {
             Font newFont = Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, fontSize--);
             text.setFont(newFont);
@@ -122,56 +90,13 @@ public class MenuCircle extends StackPane {
         getChildren().add(text);
     }
 
-
-    public Circle getCircle() {
-        return circle;
-    }
-
-    public static void setAllCircles(ArrayList<MenuCircle> allCircles) {
-        MenuCircle.allCircles = allCircles;
-    }
-
-    public Borough getBorough() {
-        return borough;
-    }
-
-    public double getRadius(){
-        return circle.getRadius();
-    }
-
-    public int getSizeValue(){
-        return borough.getAvgPrice();
-    }
-
-    public void setPosition(int x, int y){
-        setLayoutX(x);
-        setLayoutY(y);
-    }
-
-    public void setPositionFromCentre(int x, int y){
-        setLayoutX(x - getRadius());
-        setLayoutY(y - getRadius());
-
-//        setLayoutX(x - (getWidth()/2));
-//        setLayoutY(y - (getHeight()/2));
-
-//        setTranslateX(x - getRadius());
-//        setTranslateY(y - getRadius());
-    }
-
-    public double getLayoutCentreX(){
-        return getLayoutX() + getRadius();
-    }
-    public double getLayoutCentreY(){
-        return getLayoutY() + getRadius();
-    }
     /**
      * Calculates what the colour of the circle should be based on the price.
      * @return the calculated colour of the circle.
      */
     private Color calculateColor(){
-        double min = borough.getAvgPriceStat().min();
-        double max = borough.getAvgPriceStat().max();
+        double min = Borough.avgPriceStorage.min();
+        double max = Borough.avgPriceStorage.max();
 
         double calc = (borough.getAvgPrice() - min) / (max - min);
 
@@ -191,8 +116,8 @@ public class MenuCircle extends StackPane {
      * @return the calculated size of the circle.
      */
     private int calculateSize(){
-        double min = borough.getAvailableStat().min();
-        double max = borough.getAvailableStat().max();
+        double min = Borough.availableStorage.min();
+        double max = Borough.availableStorage.max();
 
         double calc;
         calc = (borough.getAvailable() - min) / (max - min);
@@ -202,26 +127,91 @@ public class MenuCircle extends StackPane {
         return (int)calc;
     }
 
-//    private void moveToMouse(MouseEvent event){
-//        setManaged(false);
-//        this.setTranslateX(event.getX() + this.getTranslateX() - (getWidth()/2));// - 120);
-//        this.setTranslateY(event.getY() + this.getTranslateY()- (getHeight()/2));// - 50);
-//        event.consume();
-//
-//        System.out.println(checkCollision(this) + " for " + borough.getName());
-//    }
+    /**
+     * Makes the cursor a hand and plays transitions on mouse enter.
+     * @param event MouseEvent call.
+     */
+    private void mouseEnter(MouseEvent event){
+        setCursor(Cursor.HAND);
+        filltCircleIn.play();
+        stTextIn.play();
+    }
 
-//    //https://stackoverflow.com/a/15014709/11245518
-//    private boolean checkCollision(MenuCircle circle) {
-//        boolean collisionDetected = false;
-//        for (MenuCircle static_circle : allCircles) {
-//            if (static_circle != circle) {
-//                Shape intersect = Shape.intersect(circle.getCircle(), static_circle.getCircle());
-//                if (intersect.getBoundsInLocal().getWidth() != -1) {
-//                    collisionDetected = true;
-//                }
-//            }
-//        }
-//        return collisionDetected;
-//    }
+    /**
+     * Makes the cursor default and reverts transitions on mouse exit.
+     * @param event MouseEvent call.
+     */
+    private void mouseLeave(MouseEvent event){
+        setCursor(Cursor.DEFAULT);
+        filltCircleOut.play();
+        stTextOut.play();
+    }
+
+    /**
+     * Opens the inspection window specific to the borough and its listings.
+     * @param event MouseEvent call.
+     */
+    private void openInspectionWindow(MouseEvent event){
+       InspectMenuController.create(listings, borough.getName());
+    }
+
+    /**
+     * Get the circle object of the object.
+     * @return The circle object of the object.
+     */
+    public Circle getCircle() {
+        return circle;
+    }
+
+    /**
+     * Get the radius of the circle.
+     * @return the radius of the circle.
+     */
+    public double getRadius(){
+        return circle.getRadius();
+    }
+
+    /**
+     * Get the value that represents the size of the circle. (The average price in the borough)
+     * @return the value that represents the size of the circle. (The average price in the borough)
+     */
+    public int getSizeValue(){
+        return borough.getAvgPrice();
+    }
+
+    /**
+     * Set the position of both X and Y of the object.
+     * @param x X position to be set.
+     * @param y Y position to be set.
+     */
+    public void setPosition(int x, int y){
+        setLayoutX(x);
+        setLayoutY(y);
+    }
+
+    /**
+     * Set the position of both X and Y of the object from the centre of the object.
+     * @param x X centre position to be set.
+     * @param y Y centre position to be set.
+     */
+    public void setPositionFromCentre(int x, int y){
+        setLayoutX(x - getRadius());
+        setLayoutY(y - getRadius());
+    }
+
+    /**
+     * Get the centre X of the object.
+     * @return the centre X of the object.
+     */
+    public double getLayoutCentreX(){
+        return getLayoutX() + getRadius();
+    }
+
+    /**
+     * Get the centre Y of the object.
+     * @return the centre Y of the object.
+     */
+    public double getLayoutCentreY(){
+        return getLayoutY() + getRadius();
+    }
 }
