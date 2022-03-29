54753772ccc18119fc7afad969be5a0632584810
Serge Alkhalil <syraj.alkhalil7@gmail.com>
2022-03-30 00:05:35 +0100

    bug fixed

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -4,12 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
