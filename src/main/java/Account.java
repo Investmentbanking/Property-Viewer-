8727ea5f5930322500fb08fa8b964fa9ff5cf9c4
Serge Alkhalil <syraj.alkhalil7@gmail.com>
2022-03-29 14:02:20 +0100

    removed the use of Date - currently using LocalDate + change of the build.gradle to remove redundant libs

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,14 +1,13 @@
 import com.opencsv.CSVReader;
 import javafx.scene.control.Alert;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Account {
     private String username;                                                        // the username
@@ -40,25 +39,20 @@ public class Account {
             String[] line;
 
             // actual dates set by the user
-            Date preferredStartDate = new Date(RuntimeDetails.getStartDate().getYear() - 1900, RuntimeDetails.getStartDate().getMonthValue() - 1, RuntimeDetails.getStartDate().getDayOfMonth()+1);
-            Date preferredEndDate   = new Date(RuntimeDetails.getEndDate().getYear() - 1900, RuntimeDetails.getEndDate().getMonthValue() - 1, RuntimeDetails.getEndDate().getDayOfMonth()+1);
+            LocalDate preferredStartDate = RuntimeDetails.getStartDate();
+            LocalDate preferredEndDate   = RuntimeDetails.getEndDate();
 
             //skip the first row (column headers)
             reader.readNext();
             while ((line = reader.readNext()) != null) {
                 String propertyID = line[0];
-                Date startDate = convertDate(line[2]);
-                Date endDate = convertDate(line[3]);
+                LocalDate startDate = convertDate(line[2]);
+                LocalDate endDate = convertDate(line[3]);
 
                 if(property.getId().equals(propertyID)){
-                    System.out.println(preferredStartDate.toInstant());
-                    System.out.println(preferredEndDate.toInstant());
-                    System.out.println(startDate.toInstant());
-                    System.out.println(endDate.toInstant());
-
                     // check there isn't a date clash
                     // if these are true we don't book they can't be simplified.
-                    if ((preferredStartDate.before(startDate) && preferredEndDate.after(startDate)) || (preferredStartDate.before(endDate) && preferredEndDate.after(endDate)) || (preferredStartDate.after(startDate) && preferredEndDate.before(endDate)) || (preferredStartDate.equals(startDate) || preferredEndDate.equals(endDate))){
+                    if ((preferredStartDate.isBefore(startDate) && preferredEndDate.isAfter(startDate)) || (preferredStartDate.isBefore(endDate) && preferredEndDate.isAfter(endDate)) || (preferredStartDate.isAfter(startDate) && preferredEndDate.isBefore(endDate)) || (preferredStartDate.equals(startDate) || preferredEndDate.equals(endDate))){
                         // then book
                         new Alerts(Alert.AlertType.WARNING,"Warning", null, "this property is already reserved in this time");
                         return true;
@@ -80,10 +74,9 @@ public class Account {
         return false;
     }
 
-    private Date convertDate(String dateString){
+    private LocalDate convertDate(String dateString){
         // date should be the same as date we have when we write
-//        String dateWeHave = String.valueOf(new Date(2020 - 1900, 1 - 1, 7).toInstant());
-        return new Date(Integer.valueOf(dateString.substring(0, 4)) - 1900, Integer.valueOf(dateString.substring(5, 7))-1, Integer.valueOf(dateString.substring(8, 10)) + 1);
+        return LocalDate.of(Integer.parseInt(dateString.substring(0, 4)), Integer.parseInt(dateString.substring(5, 7)), Integer.parseInt(dateString.substring(8, 10)));
     }
 
     /**
@@ -94,9 +87,9 @@ public class Account {
     private void bookProperty(NewAirbnbListing property){
         try {
             FileWriter writer = new FileWriter(file, true);
-            Date preferredStartDate = new Date(RuntimeDetails.getStartDate().getYear() - 1900, RuntimeDetails.getStartDate().getMonthValue() - 1, RuntimeDetails.getStartDate().getDayOfMonth()+1);
-            Date preferredEndDate   = new Date(RuntimeDetails.getEndDate().getYear() - 1900, RuntimeDetails.getEndDate().getMonthValue() - 1, RuntimeDetails.getEndDate().getDayOfMonth()+1);
-            String newBooking = (property.getId()) + "," + MainController.getCurrentUser().getUsername() + "," + preferredStartDate.toInstant() + ","  + preferredEndDate.toInstant() + '\n';
+            LocalDate preferredStartDate = RuntimeDetails.getStartDate();
+            LocalDate preferredEndDate   = RuntimeDetails.getEndDate();
+            String newBooking = (property.getId()) + "," + MainController.getCurrentUser().getUsername() + "," + preferredStartDate + ","  + preferredEndDate + '\n';
             writer.write(newBooking);
             writer.close();
         }catch (Exception e){
